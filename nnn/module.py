a0a79e15947080f2b0eecf8221e9f5573ad361e3
Yuta <52748151+yutanagano@users.noreply.github.com>
2022-12-28 12:54:30 +0900

    Finish chapter 20

--- nnn/module.py
+++ nnn/module.py
@@ -183,41 +183,56 @@ class Module:
         
             # If there is the validation data
             if validation_data is not None:
-                # Reset accumulated values in loss
-                # and accuracy objects
-                self.loss.new_pass()
-                self.accuracy.new_pass()
-                
-                # Iterate over steps
-                for step in range(validation_steps):
-                    # If batch size is not set -
-                    # train using one step and full dataset
-                    if batch_size is None:
-                        batch_X = X_val
-                        batch_y = y_val
-                    # Otherwise slice a batch
-                    else:
-                        batch_X = X_val[
-                        step*batch_size:(step+1)*batch_size
-                        ]
-                        batch_y = y_val[
-                        step*batch_size:(step+1)*batch_size
-                        ]
+                self.evaluate(*validation_data, batch_size=batch_size)
+    
 
-                    # Perform the forward pass
-                    output = self.forward(batch_X, training=False)
-                    # Calculate the loss
-                    self.loss.calculate(output, batch_y)
-                    # Get predictions and calculate an accuracy
-                    predictions = self.output_layer_activation.predict(
-                    output)
-                    self.accuracy.calculate(predictions, batch_y)
+    def evaluate(self, X_val, y_val, *, batch_size=None):
+        # Default value if batch size is not being set
+        validation_steps = 1
+        # Calculate number of steps
+        if batch_size is not None:
+            validation_steps = len(X_val) // batch_size
+            # Dividing rounds down. If there are some remaining
+            # data, but not a full batch, this won't include it
+            # Add `1` to include this not full batch
+            if validation_steps * batch_size < len(X_val):
+                validation_steps += 1
+        
+        # Reset accumulated values in loss
+        # and accuracy objects
+        self.loss.new_pass()
+        self.accuracy.new_pass()
+        
+        # Iterate over steps
+        for step in range(validation_steps):
+            # If batch size is not set -
+            # train using one step and full dataset
+            if batch_size is None:
+                batch_X = X_val
+                batch_y = y_val
+            # Otherwise slice a batch
+            else:
+                batch_X = X_val[
+                step*batch_size:(step+1)*batch_size
+                ]
+                batch_y = y_val[
+                step*batch_size:(step+1)*batch_size
+                ]
 
-                # Get and print validation loss and accuracy
-                validation_loss = self.loss.calculate_accumulated()
-                validation_accuracy = self.accuracy.calculate_accumulated()
+            # Perform the forward pass
+            output = self.forward(batch_X, training=False)
+            # Calculate the loss
+            self.loss.calculate(output, batch_y)
+            # Get predictions and calculate an accuracy
+            predictions = self.output_layer_activation.predict(
+            output)
+            self.accuracy.calculate(predictions, batch_y)
 
-                # Print a summary
-                print(f'validation, ' +
-                f'acc: {validation_accuracy:.3f}, ' +
-                f'loss: {validation_loss:.3f}')
+        # Get and print validation loss and accuracy
+        validation_loss = self.loss.calculate_accumulated()
+        validation_accuracy = self.accuracy.calculate_accumulated()
+
+        # Print a summary
+        print(f'validation, ' +
+        f'acc: {validation_accuracy:.3f}, ' +
+        f'loss: {validation_loss:.3f}')
